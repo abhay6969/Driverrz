d52c5b95fc3b33f14b956c8995a9880050c5dabd
abhay6969 <abhaytripathi6969@gmail.com>
2025-07-08 12:52:23 +0530

    navbar complete

--- src/components/homeComponents/heroSection.tsx
+++ src/components/homeComponents/heroSection.tsx
@@ -1,10 +1,16 @@
+import main1 from "../../assets/main1.png";
 export const HeroSection = ()=>{
     return(
         <div className="w-full h-screen bg-cover bg-center relative">
-            <div >
-               <h1>Welcome to Chalo Ghoomne</h1>
-            <p>Your journey starts here</p>
-            <button>Get Started</button> 
+            <div className="w-full flex flex-row justify-center items-center h-full bg-black bg-opacity-50">
+               <div className="w-1/2 flex flex-col justify-center items-center border-2 border-gray-300 rounded-lg">
+                     <h1 className="text-5xl text-white font-bold mb-4">Welcome to Driverrz</h1>
+                     <p className="text-lg text-gray-300 mb-8">Your one-stop solution for all your travel needs.</p>
+                     <button className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300">Get Started</button>
+               </div>
+               <div className="w-1/2 h-1/2 border-2 border-gray-300 rounded-lg overflow-hidden">    
+                <img src={main1} alt="" className="w-full h-full object-cover overflow-x-clip "/>
+               </div>
             </div>
             
         </div>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,19 +1,88 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { AiFillCaretDown } from "react-icons/ai";
+import { Link } from "react-router";
 
-export const Navbar = ()=>{
-    const links = [
-        { name: "Home", path: "/" },
-        { name: "About", path: "/about" },
-        { name: "Services", path: "/services" },
-        { name: "Contact", path: "/contact" }
-    ];
-    return(
-        <div >
-            <div className=" bg-gray-500 text-white p-4 flex justify-between items-center">
-                {links.map((link) =>(
-                    <Link className="" key={link.name} to={link.path}>{link.name}</Link>
-                ))}
-            </div>
-        </div>
-    )
-}
+export const Navbar = () => {
+	const [servicesActive, setServicesActive] = useState("");
+	const links = [
+		{ name: "Home", path: "/" },
+		{ name: "About", path: "/about" },
+		{ name: "Services", path: "/services", hasDropdown: true, type: "service" },
+        { name: "City", path: "/services", hasDropdown: true, type: "city"  },
+		{ name: "Contact", path: "/contact" },
+	];
+
+	const servicesData = [
+  {
+    type: "service",
+    items: [
+      { name: "All Services", path: "/services/web-development" },
+      { name: "Daily Services", path: "/services/app-development" },
+      { name: "Permanent Services", path: "/services/seo" },
+      { name: "Outstation Services", path: "/services/seo" },
+      { name: "Temporary Services", path: "/services/seo" },
+      { name: "Hourly Services", path: "/services/seo" },
+      { name: "Night Services", path: "/services/seo" },
+    ],
+  },
+  {
+    type: "city",
+    items: [
+      { name: "Kanpur", path: "#" },
+      { name: "Lucknow", path: "#" },
+      { name: "Unnao", path: "#" },
+    ],
+  },
+];
+
+	return (
+		<div className=" bg-gray-500 text-white p-4 flex justify-center gap-6 items-center relative">
+			{links.map((link) => {
+				if (link.hasDropdown) {
+                    const dropdownData = servicesData.find(
+            (group) => group.type === link.type
+          );
+					return (
+						<div
+							key={link.name}
+							className="relative"
+							onMouseEnter={() => setServicesActive(link.name)}
+							onMouseLeave={() => setServicesActive("")}
+						>
+							<Link
+								to="#"
+								className="flex flex-row gap-1 items-center">
+								{link.name}{" "}
+								<AiFillCaretDown
+                  className={`transition-transform duration-200 ${
+                    servicesActive === link.name ? "rotate-180" : "rotate-0"
+                  }`}
+                />
+							</Link>
+							{servicesActive === link.name && dropdownData    && (
+								<div className="bg-gray-500 m-3 p-2 w-40  absolute z-10">
+									<div className="flex flex-col gap-1 p-2">
+										{dropdownData.items.map((service) => (
+											<Link
+												key={service.name}
+												to={service.path}
+											>
+												{service.name}
+											</Link>
+										))}
+									</div>
+								</div>
+							)}
+						</div>
+					);
+				} else {
+					return (
+						<Link className="" key={link.name} to={link.path}>
+							{link.name}
+						</Link>
+					);
+				}
+			})}
+		</div>
+	);
+};
